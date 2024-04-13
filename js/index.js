061a37c911acb236b31c8ec09f2eb3f96183217c
yugami10 <yugami333@gmail.com>
2024-04-13 12:35:18 +0900

    reason

--- js/index.js
+++ js/index.js
@@ -17,12 +17,28 @@ const clickHambarger = () => {
     switchHambargerMenu()
 }
 
+const scrollEvents = () => {
+    const reason__left_item = document.querySelector('.reason__left_item')
+    const online_item_position = reason__left_item.getBoundingClientRect()
+    if (online_item_position.top < window.innerHeight) {
+        reason__left_item.classList.add('slide-left')
+    }
+
+    const reason__right_item = document.querySelector('.reason__right_item')
+    const teacher_item_position = reason__right_item.getBoundingClientRect()
+    if (teacher_item_position.top < window.innerHeight) {
+        reason__right_item.classList.add('slide-right')
+    }
+}
+
 /**
  * 初回ロード時に実行
  */
 const domContentLoaded = () => {
     const hambarger = document.querySelector('.hambarger_icon')
     hambarger.addEventListener('click', clickHambarger)
+
+    document.addEventListener('scroll', scrollEvents)
 }
 
 document.addEventListener('DOMContentLoaded', domContentLoaded)
